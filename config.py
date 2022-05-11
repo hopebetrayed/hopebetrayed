34b5c1ef92e4ee5958f6b826a8de4dde8bea2d5a
hopebetrayed <105384400+hopebetrayed@users.noreply.github.com>
2022-05-11 21:07:07 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# Конфиг
+command_prefixes = ['.','!','/']
+
+ghoul_table_command = 'ghoul-c'
+
+end_message = 'by https://t.me/hope_betrayed' # Сообщение после конца цикла, если не нужно - оставляем пустым
+messages_per_second = 7 
+sleep_time_ghoul = 0.1 
+
+# Линии ниже не трогать, сломается!
+
+api_id = 1016382
+api_hash = 'c27834e5683d50a9bacf835a95ec4763'
+
+# Линии выше не трогать, сломается!
